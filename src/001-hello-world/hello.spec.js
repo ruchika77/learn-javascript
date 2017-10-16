3928226e62893258446bcde9976e7332007f8c21
ruchika77 <siva3378@gmail.com>
2017-10-16 23:27:25 +0100

    node project for TDD

---
+++ src/001-hello-world/hello.spec.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+
+var helloWorld = require('./hello.js');
+
+describe('Hello World - function', function(){
+
+    it('should be defined', function(){
+
+        expect(helloWorld).to
+
+    });
+
+});
